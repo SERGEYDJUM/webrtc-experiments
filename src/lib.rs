2c7dd1371aa7a5f4ad73423af35d87cd6212af8e
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2024-09-13 13:35:32 +0300

    Init boilerplate

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod utils;
+
+use utils::set_panic_hook;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn run() -> Result<(), JsValue> {
+    set_panic_hook();
+    alert("Hello, webrt-experiments!");
+    Ok(())
+}
